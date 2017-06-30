0545b0fd7284f9e8e12df94c21d9f798c987d678
Tim Wieschalla <Tim.Wieschalla@gmail.com>
2017-06-30 12:17:55 +0200

    add rerun script after instance stop

---
+++ images/master/bin/container-rerun.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+docker run \
+    -d \
+    -p 8080:8080 \
+    -p 50000:50000 \
+    -v $(which docker):/usr/bin/docker \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /root/workspace \
+    jenkins-master &&\
+    docker logs -f jenkins-master;
